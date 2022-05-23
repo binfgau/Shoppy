6b253a9ffb7c1f77e42bf861ff237dc6bcac5290
Gấu <nguyenvanbinh13112000@outlook.com>
2022-05-23 20:30:57 +0700

    done context api

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ import {
   Line,
 } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
