94988d7ba26bd9da36ee48c6feb9f00ecb18957a
Gấu <nguyenvanbinh13112000@outlook.com>
2022-05-24 10:22:34 +0700

    change class for link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,9 +12,9 @@ const Sidebar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink =
-    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 text-white';
   const normalLink =
-    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray';
 
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
@@ -26,7 +26,8 @@ const Sidebar = () => {
               onClick={() => setActiveMenu(false)}
               className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
             >
-              <SiShopware /> <span>Shoppy</span>
+              <SiShopware />
+              <span>Shoppy</span>
             </Link>
             <TooltipComponent content='Menu' position='BottomCenter'>
               <button
